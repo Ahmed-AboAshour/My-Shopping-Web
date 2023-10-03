bee63c178ceb92416b28f89009a6dfd545dc8c1c
Ahmed-AboAshour <ahmedashour159@gmail.com>
2023-10-03 12:19:30 +0200

    first commit

---
+++ src/products.js
@@ -0,0 +1,68 @@
+import product1 from './assets/1.png';
+import product2 from './assets/2.png';
+import product3 from './assets/3.png';
+import product4 from './assets/4.png';
+import product5 from './assets/5.png';
+import product6 from './assets/6.png';
+import product7 from './assets/7.png';
+import product8 from './assets/8.png';
+import product9 from './assets/9.png';
+
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: "Mercedes",
+        price: 150000.0,
+        productImage: product1,
+    },
+    {
+        id: 2,
+        productName: "BMW",
+        price: 80000.0,
+        productImage: product2,
+    },
+    {
+        id: 3,
+        productName: "Ferrari",
+        price: 350000.0,
+        productImage: product3,
+    },
+    {
+        id: 4,
+        productName: "Lamborghini",
+        price: 450000.0,
+        productImage: product4,
+    },
+    {
+        id: 5,
+        productName: "Porsche",
+        price: 175000.0,
+        productImage: product5,
+    },
+    {
+        id: 6,
+        productName: "Rolls Royce",
+        price: 500000.0,
+        productImage: product6,
+    },
+    {
+        id: 7,
+        productName: "Ford Mustang",
+        price: 30000.0,
+        productImage: product7,
+    },
+    {
+        id: 8,
+        productName: "Bentely",
+        price: 350000.0,
+        productImage: product8,
+    },
+    {
+        id: 9,
+        productName: "Range Rover",
+        price: 90000.0,
+        productImage: product9,
+    },
+];
